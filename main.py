54a4ee6e4e035f2fd8d0d80b2ea8c5ca5169e8c5
Montecchia <emontecchia@frba.utn.edu.ar>
2024-05-08 21:42:43 -0300

    Añadido filtro por relleno

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from toga.style.pack import COLUMN, LEFT, RIGHT, ROW
 import adbb
 import relleno
 
-
 user = "montecchiacorp"
 password = "k-gpQzCw-uU5zQf"
 sql = "sqlite:///adbb.db"
@@ -18,7 +17,7 @@ ids = [numero[0] for numero in animes]
 
 def agregar_serie(tabla_series, nombre):
     anime = adbb.Anime(nombre)
-    tiene_relleno = False
+    capitulos_relleno = 0
     titulo_anime = anime.title
     for titulo in anime.titles:
         if titulo.lang == "eng" and titulo.titletype == "official":
@@ -26,32 +25,46 @@ def agregar_serie(tabla_series, nombre):
             break
 
     link_relleno = relleno.buscar_serie(titulo_anime)
+
     if link_relleno is not None:
-        tiene_relleno = True
+        capitulos_relleno = len(list(filter(lambda x: x < anime.highest_episode_number,
+                                            relleno.lista_relleno(link_relleno))))
 
     tabla_series.data.append({"título": titulo_anime,
                               "capítulos": anime.highest_episode_number,
-                              "relleno": tiene_relleno,
+                              "relleno": capitulos_relleno,
                               "link": link_relleno})
 
 
-def llenar_capitulos(tabla_capitulos, nombre_anime, url_relleno):
+def llenar_capitulos(tabla_capitulos, nombre_anime, url_relleno, filtrar_relleno, barra_carga):
     anime = adbb.Anime(nombre_anime)
     lista_relleno = relleno.lista_relleno(url_relleno)
+    barra_carga.max = anime.highest_episode_number+1
     for numero in range(1, anime.highest_episode_number+1):
+        barra_carga.value = numero
         es_relleno = "Canon"
         episodio = adbb.Episode(anime=anime, epno=numero)
         if numero in lista_relleno:
                 es_relleno = "Relleno"
+                if filtrar_relleno:
+                    continue
         tabla_capitulos.data.append([episodio.epno, episodio.title_eng, False, es_relleno])
         tabla_capitulos.refresh()
 
 
 class SeguidorSeries (toga.App):
-
+    current_row = None
     def cargar_capitulos(self, *args, row):
-        self.tabla_capitulos.data.clear()
-        llenar_capitulos(self.tabla_capitulos, row.título, row.link)
+        if row is not None:
+            self.tabla_capitulos.data.clear()
+            self.barra_carga.start()
+            llenar_capitulos(self.tabla_capitulos,
+                             row.título, row.link,
+                             self.filtrar_relleno.value,
+                             self.barra_carga)
+            self.barra_carga.value = 0
+            self.barra_carga.stop()
+            self.current_row = row
 
     def alternar_visto(self, *args, row):
         if row.visto:
@@ -64,7 +77,7 @@ class SeguidorSeries (toga.App):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
         box_ingresos = toga.Box(style=Pack(direction=COLUMN, flex=1))
-        box_tablas = toga.Box(style=Pack(direction=ROW, flex=2))
+        box_tablas = toga.Box(style=Pack(direction=ROW, flex=5))
 
         self.tabla_capitulos = toga.Table(
             headings=["Capítulo", "Título", "Visto", "Tipo"],
@@ -102,8 +115,20 @@ class SeguidorSeries (toga.App):
 
         input_button = toga.Button("Iniciar", on_press=iniciar())
 
+        self.filtrar_relleno = toga.Switch(
+            "Ocultar relleno",
+            value=False,
+            on_change=self.cargar_capitulos(row=self.current_row))
+
+        self.barra_carga = toga.ProgressBar(style=Pack(flex=1))
+
+        box_checks = toga.Box(style=Pack(direction=ROW, padding=2))
+
         box_ingresos.add(box_nombre)
         box_ingresos.add(input_button)
+        box_checks.add(self.filtrar_relleno)
+        box_checks.add(self.barra_carga)
+        box_ingresos.add(box_checks)
 
         main_box.add(box_tablas)
         main_box.add(box_ingresos)
